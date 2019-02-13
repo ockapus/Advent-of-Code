521391c22128c0854c10d16bf18d6680c9b98b71
Chris Mangum <ockapus@users.noreply.github.com>
2019-02-13 10:21:20 -0800

    Solutions for Day 4 (#4)

---
+++ 2018/Day 04/part2.rb
@@ -0,0 +1,54 @@
+# Advent of Code 2018, Day 4
+# Part 2: Sorting the Schedule
+
+entries = []
+File.open("day04_input.txt").each do |line|
+    entries << line.chomp
+end
+
+# Sort by the timestamps before processing
+entries.sort_by! { |line| line.scan(/\d{4}-\d{2}-\d{2} \d{2}:\d{2}/)[0] }
+
+# Now process entries in order. We don't care about the actual days
+# except as a marker that we've got a new set of entries, so we're just
+# going to mark up total times each guard is asleep for a minute during
+# the midnight hour
+guards = {}
+awake = true
+last_change = 0
+current_guard = 0
+entries.each do |entry|
+    tokens = /^\[\d{4}-\d{2}-\d{2} \d{2}:(\d{2})\] (.*)$/.match(entry)
+    minute = tokens[1].to_i
+    event = tokens[2]
+
+    if event == 'falls asleep'
+        awake = false
+        last_change = minute
+    elsif event == 'wakes up'
+        awake = true
+        (last_change..minute-1).each do |m|
+            guards[current_guard][m] += 1
+        end
+    else
+        current_guard = /^Guard #(\d+) begins shift/.match(event)[1]
+        if !guards.key?(current_guard)
+            guards[current_guard] = Array.new(60, 0)
+        end
+    end
+end
+
+worst_guard = nil
+worst_minute = nil
+worst_time = -1
+guards.each do |guard, minutes|
+    (0..59).each do |m|
+        if minutes[m] > worst_time
+            worst_time = minutes[m]
+            worst_minute = m
+            worst_guard = guard
+        end
+    end
+end
+
+puts "Guard ID times worst minute: " + worst_guard + 'x' + worst_minute.to_s + ' = ' + (worst_guard.to_i * worst_minute).to_s
